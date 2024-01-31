9c549650030183ab4f0cf30f5932a8ec5a4da93b
KwakDongyoung <wlsdlseowk@gmail.com>
2024-01-31 16:34:25 +0900

    [Feat] 추천 기능 수정중

--- IPAD/WebContent/js/locationRecommand.js
+++ IPAD/WebContent/js/locationRecommand.js
@@ -1,4 +1,59 @@
-function getRankList(){
-    
-    
+function writeRankList() {
+    getRankList();
+
+    document.getElementById('first').innerText = list[0];
+    document.getElementById('second').innerText = list[1];
+    document.getElementById('third').innerText = list[2];
 }
+var list = [];
+
+function getRankList() {
+    var checkImpl = document.getElementById('implant').checked;
+    var checkOrth = document.getElementById('orthodontics').checked;
+    console.log(checkImpl);
+    console.log(checkOrth);
+    var formData = new FormData();
+    formData.append("checkImpl", checkImpl);
+    formData.append("checkOrth", checkOrth)
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", contextPath + "/locationRecommand/submit.do", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.status == 200) {
+            var result = JSON.stringify(xhr.responseText);
+            console.log(result);
+        }
+    };
+    xhr.send(formData);
+    //    var jsonData = new Object();
+    //    jsonData.key1 = checkImpl;
+    //    jsonData.key2 = checkOrth;
+    //
+    //    var sendData = JSON.stringify(jsonData);
+    // $.ajax({
+    //     url: contextPath + "/json/locationRecommand.do" + "?data=" + encodedURIComponent(sendData),
+    //     success: function (retData) {
+    //         var resObj = JSON.parse(retData)
+
+    //     },
+    //     error: function (error) {
+    //         console.log(error);
+    //     }
+
+    // })
+
+    // fetch(contextPath + "/json/locationRecommand.do")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         for (let i = 0; i < data.length; i++) {
+    //             list.push(data[i]["adm_nm"]);
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error('Error :', error);
+    //     });
+}
+
+
--- IPAD/src/com/ipad/service/locationRecommand/LocationRecommandService.java
+++ IPAD/src/com/ipad/service/locationRecommand/LocationRecommandService.java
@@ -1,10 +1,15 @@
 package com.ipad.service.locationRecommand;
 
+
+import java.io.PrintWriter;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.ipad.dao.locationRecommand.LocationRecommandDao;
 import com.ipad.dto.locationRecommand.LocationRecommandDto;
 import com.ipad.service.Service;
@@ -14,18 +19,32 @@ public class LocationRecommandService implements Service {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {		
 		LocationRecommandDao dao = new LocationRecommandDao();
-		String opt1 =request.getParameter("implant");
-		String opt2 =request.getParameter("orthodontics");
-		
+		System.out.println(request.getParameter("checkImpl"));
+		System.out.println(request.getParameter("checkOrth"));
+		boolean opt1 = Boolean.parseBoolean(request.getParameter("checkImpl"));
+		boolean opt2 = Boolean.parseBoolean(request.getParameter("checkOrth"));
+		System.out.println(opt1+" " + opt2);
+//		System.out.println(request.getParameter("checkImpl").getClass());
 		ArrayList<LocationRecommandDto> dtos = dao.selectRegion();
 		
 		for(int i=0; i<dtos.size();i++) {
 			dao.setSaleScore(dtos.get(i));
 			dao.setTwentiesScore(dtos.get(i));
 			dao.setSixtiesScore(dtos.get(i));
-			dao.setTotalScore(dtos.get(i),opt2,opt1);
+			dao.setTotalScore(dtos.get(i),opt1,opt2);
 		}
-		request.setAttribute("rank", dao.getTop3List(dtos));
+		List<LocationRecommandDto> rankList = dao.getTop3List(dtos);
+		String jsonResponse = new Gson().toJson(rankList);
+
+		response.setContentType("application/json; charset=utf-8");
+		response.setCharacterEncoding("UTF-8");
+		request.setAttribute("rank", rankList);
+		try(PrintWriter out = response.getWriter()){
+			out.print(jsonResponse);
+			out.flush();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 
 	}
